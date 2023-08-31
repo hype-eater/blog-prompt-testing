55134bd4918ec9b723fc609b705f89d8cf704fd4
Hype Eater <10122729+hype-eater@users.noreply.github.com>
2023-08-31 01:06:34 -0700

    add llm usage

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
 import typer
+from langchain import OpenAI, PromptTemplate
 
 app = typer.Typer(help="A helpful and friendly CLI")
+
+GREETING_PROMPT = "A greeting: "
+INTRODUCTION_PROMPT_TMPL = PromptTemplate(
+    input_variables=["name"],
+    template="Hello, my name is {name}.",
+)
 
 
 @app.command()
 def hello():
     """Say hello"""
-    print("Hello")
+    executor = get_prompt_executor()
+    response = executor(GREETING_PROMPT)
+    print(response.strip())
 
 
 @app.command()
 def intro(name: str = "User"):
     """Reply to an introduction"""
-    print(f"Hi, {name}")
+    executor = get_prompt_executor()
+    prompt = INTRODUCTION_PROMPT_TMPL.format_prompt(name=name)
+    response = executor(prompt.to_string())
+    print(response.strip())
+
+
+def get_prompt_executor(model_name="text-davinci-003"):
+    executor = OpenAI(model_name=model_name)
+    return executor
 
 
 if __name__ == '__main__':
