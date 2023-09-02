0c5e8f39a1a86210b221e61c767a606f0b1e11bf
Hype Eater <10122729+hype-eater@users.noreply.github.com>
2023-09-01 18:42:21 -0700

    test case comprehensions

--- prompt_cases.py
+++ prompt_cases.py
@@ -5,22 +5,28 @@ from app import GREETING_PROMPT, INTRODUCTION_PROMPT_TMPL, get_prompt_executor
 
 prompt_executor = get_prompt_executor()
 
-simple_prompt_cases = [
-    PromptCase(GREETING_PROMPT,
-               key="greeting",
-               category="greeting",
-               evaluators=[lambda x: evals.any_word(x.response, ["hello", "hi", "hey", "hiya"])],
-               prompt_executor=prompt_executor,
-               ),
-    ]
+def greeting_case(prompt_executor, index):
+    greeting_words = ["hello", "hi", "hey", "hiya"]
+    return PromptCase(GREETING_PROMPT,
+                      key=f"greeting-{index}",
+                      category="greeting",
+                      evaluators=[lambda x: evals.any_word(x.response, greeting_words)],
+                      prompt_executor=prompt_executor,
+                      )
 
-intro_tmpl_cases = [
-    LangchainPromptCase(INTRODUCTION_PROMPT_TMPL,
-                        key="intro-kitty",
-                        category="intro",
-                        name="Kitty",
-                        evaluators=[lambda x: evals.all_words(x.response, ["Kitty", ]),
-                                    lambda x: evals.any_word(x.response, ["nice", "hi", "hello"])],
-                        prompt_executor=prompt_executor,
-                        ),
-]
+
+simple_prompt_cases = [greeting_case(prompt_executor, index) for index in range(5)]
+
+def intro_case_for_name(prompt_executor, name: str) -> LangchainPromptCase:
+    return LangchainPromptCase(INTRODUCTION_PROMPT_TMPL,
+                               key=f"intro-{name.lower()}",
+                               category="intro",
+                               name=name,
+                               evaluators=[lambda x: evals.all_words(x.response, [name, ]),
+                                           lambda x: evals.any_word(x.response, ["nice", "hi", "hello"])],
+                               prompt_executor=prompt_executor,
+                               )
+
+
+names = ["Kitty", "bob", "you", "ME", "who"]
+intro_tmpl_cases = [intro_case_for_name(prompt_executor, name) for name in names]
